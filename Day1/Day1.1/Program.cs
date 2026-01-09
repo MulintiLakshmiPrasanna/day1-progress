43bdea1b0c65ed2cc4e374e7a5f17faca9a7ac25
MulintiLakshmiPrasanna <mulintilakshmiprasanna@gmail.com>
2026-01-09 08:51:28 +0530

    Added Day 1and Day1.1 c# console programs

---
+++ Day1/Day1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your age:");
+
+string ageInput = Console.ReadLine();
+int age;
+
+bool isValidAge = int.TryParse(ageInput, out age);
+
+if (isValidAge)
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("You are eligible to vote.");
+    }
+    else
+    {
+        Console.WriteLine("You are not eligible to vote.");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter a valid number.");
+}
+
